ac2ae82ecdb7d00bb7df9e04f927406bcae37daa
Student <student@bluc02mf24vfh51.local>
2016-01-20 12:03:07 -0600

    set up tableview complete

--- colledgeBuilder/ViewController.swift
+++ colledgeBuilder/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
-    var colleges = ["UOI", "NIU", "CLC"]
+    var colleges = ["UOI", "NIU", "CLC"];
     
     override func viewDidLoad()
     {
-        super.viewDidLoad()
+        super.viewDidLoad();
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return colleges.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath);
+        cell.textLabel?.text = colleges[indexPath.row];
+        return cell;
+    }
     
 }
 
