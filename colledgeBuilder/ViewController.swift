e4910dda0d0f4943a4ff344627df9b0c2e8b23f7
Student <student@bluc02mf24vfh51.local>
2016-01-20 11:57:34 -0600

    connect storyboard and put in protocols.

--- colledgeBuilder/ViewController.swift
+++ colledgeBuilder/ViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var colleges = ["UOI", "NIU", "CLC"]
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
 }
 
