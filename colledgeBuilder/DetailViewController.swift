d3bcfa73121b8d0eebe4c22b87943fbca53db839
Student <student@bluc02mf24vfh51.local>
2016-01-25 12:28:41 -0600

    created detail view controller

---
+++ colledgeBuilder/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  colledgeBuilder
+//
+//  Created by Student on 1/25/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController
+{
+    @IBOutlet weak var collegeTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var college : College!;
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad();
+        collegeTextField.text = college.name;
+        stateTextField.text = college.state;
+        populationTextField.text = String(college.population);
+        imageView.image = college.image;
+    }
+
+    @IBAction func onTappedSaveButton(sender: AnyObject)
+    {
+        college.name = collegeTextField.text!;
+        college.state = stateTextField.text!;
+        college.population = Int(populationTextField.text!)!;
+    }
+}
